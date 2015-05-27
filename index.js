68750177b7903e824f19959137a5e5f4ce6f37d9
Spiro Sideris <spiro@mobify.com>
2015-05-27 16:00:07 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var os = require('os');
+
+var module.export = function () {
+    var interfaces = os.networkInterfaces(),
+         addresses = {};
+
+    for (var interface in interfaces) {
+        for (var i in interfaces[interface]) {
+            var netType = interfaces[interface][i];
+
+            if (netType.address != undefined &&
+                netType.family == 'IPv4' &&
+                !netType.internal
+               ) {
+
+                addresses[interface] = netType.address;
+            }
+        };
+    }
+
+    return addresses;
+}
